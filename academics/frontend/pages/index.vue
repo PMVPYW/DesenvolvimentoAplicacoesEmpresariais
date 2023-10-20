d84399f63ef1ea603cce0b795e2deb32c74859f5
Pedro Vicente <2210716@my.ipleiria.pt>
2023-10-20 17:47:10 +0100

    fix ficha3 end

--- academics/frontend/pages/index.vue
+++ academics/frontend/pages/index.vue
@@ -1,43 +1,27 @@
 <template>
-  <form @submit.prevent="create">
-    <div>Username: <input v-model="studentForm.username" type="text" /></div>
-    <div>Password:<input v-model="studentForm.password" type="password" /></div>
-    <div>Name: <input v-model="studentForm.name" type="text" /></div>
-    <div>E-mail: <input v-model="studentForm.email" type="text" /></div>
-    <div>
-      Course:
-      <select v-model="studentForm.courseCode">
-        <option v-for="course in courses" :value="course.code">
-          {{ course.name }}
-        </option>
-      </select>
-    </div>
-    <button type="reset">RESET</button>
-    <button type="submit">CREATE</button>
-    <nuxt-link to="/">Return</nuxt-link>
-  </form>
-  {{ message }}
+  <div v-if="error">Error: {{ error.message }}</div>
+  <div v-else>
+    <nuxt-link to="/create">Create a New Student</nuxt-link>
+    <h2>Students</h2>
+    <table>
+      <tr>
+        <th>Username</th>
+        <th>Name</th>
+        <th>E-mail</th>
+        <th>Curso</th>
+      </tr>
+      <tr v-for="student in students">
+        <td>{{ student.username }}</td>
+        <td>{{ student.name }}</td>
+        <td>{{ student.email }}</td>
+        <td>{{ student.courseName }}</td>
+      </tr>
+    </table>
+  </div>
+  <button @click.prevent="refresh">Refresh Data</button>
 </template>
 <script setup>
-const studentForm = reactive({
-  username: "",
-  password: "",
-  email: "",
-  name: "",
-  courseCode: "",
-});
-const message = ref("");
 const config = useRuntimeConfig();
 const api = config.public.API_URL;
-const { data: courses } = await useFetch(`${api}/courses`);
-async function create() {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(studentForm),
-  };
-  const { error } = await useFetch(`${api}/students`, requestOptions);
-  if (!error.value) navigateTo("/");
-  message.value = error.value;
-}
+const { data: students, error, refresh } = await useFetch(`${api}/students`);
 </script>
