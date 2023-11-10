e179939476350675ab7496b3019567088edd2485
Pedro Vicente <prog2002pedro@gmail.com>
2023-11-10 15:31:38 +0000

    bugfix

--- academics/backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/entities/Teacher.java
+++ academics/backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/entities/Teacher.java
@@ -1,17 +1,20 @@
 package pt.ipleiria.estg.dei.ei.dae.academics.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Teacher extends User{
     protected  String office;
     @ManyToMany(mappedBy = "teachers")
     protected List<Subject> subjects = new ArrayList<Subject>();
 
     public Teacher() {
+        super();
     }
 
     public Teacher(String username, String password, String name, String email, String office) {
