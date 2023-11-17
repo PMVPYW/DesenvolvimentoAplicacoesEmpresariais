cc5afaa94644744e3fa148cd53df56b8a14c3f91
Pedro Vicente <prog2002pedro@gmail.com>
2023-11-17 16:22:38 +0000

    bugfix

--- academics/backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/entities/Student.java
+++ academics/backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/entities/Student.java
@@ -19,17 +19,7 @@ import java.util.List;
                 query = "SELECT s FROM Subject  s ORDER BY course.name, scholarYear, courseYear, name"
         )
 })
-public class Student implements Serializable {
-    @Id
-    protected String username;
-
-    @NotNull
-    private String password;
-    @NotNull
-    private String name;
-    @NotNull
-    @Email
-    protected String email;
+public class Student extends User implements Serializable {
     @ManyToOne
     @JoinColumn(name = "course_code")
     @NotNull
@@ -38,68 +28,22 @@ public class Student implements Serializable {
     @ManyToMany(mappedBy = "students")
     protected List<Subject> subjects;
 
-    public String getName() {
-        return new String(name);
-    }
 
-    public void setName(String name) {
-        if (name == null)
-        {
-            return;
-        }
-        this.name = new String(name);
-    }
 
 
 
     public Student()
     {
-
+        super();
     }
 
-    public String getUsername() {
-        return new String(username);
-    }
 
-    public void setUsername(String username) {
-        if (username == null)
-        {
-            return;
-        }
-        this.username = new String(username);
-    }
 
-    public String getPassword() {
-        return new String(password);
-    }
 
     public Student(String username, String password, String email, String name, Course course) {
-        this.username = new String(username);
-        this.password = new String(password);
-        this.email = new String(email);
-        this.name = new String(name);
+        super(username, password, name, email);
         this.course = course;
         course.addStudent(this);
-    }
-
-    public void setPassword(String password) {
-        if (password == null)
-        {
-            return;
-        }
-        this.password = new String(password);
-    }
-
-    public String getEmail() {
-        return new String(email);
-    }
-
-    public void setEmail(String email) {
-        if (email == null)
-        {
-            return;
-        }
-        this.email = email;
     }
 
     public Course getCourse() {
